96a9040272ce3715e0035ecc0a55ae755a9ea714
Nga <38340962+nga20167304@users.noreply.github.com>
2021-10-11 19:17:34 +0700

    fix after comment

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -6,39 +6,12 @@ month = params["m"].to_i
 year = params["y"].to_i
 
 def check_month(month)
-  if month == 0
-    return Date.today.month
-  else
-    return month
-  end
+  month == 0 ? Date.today.month : month
 end
 
 def check_year(year)
-  if year == 0
-    return Date.today.year
-  else
-    return year
-  end
+  year == 0 ? Date.today.year : year
 end
-
-def days_in_month(month, year)
-  if month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12
-    return 31
-  elsif month == 4 || month == 6 || month == 9 || month == 11
-    return 30
-  elsif Date.leap?(year)
-    return 29
-    else return 28
-  end
-end
-
-_month = check_month(month)
-_year = check_year(year)
-
-number_of_days = days_in_month(_month, _year)
-
-start_day = Time.new(_year,_month,1)
-x = start_day.wday
 
 def format_calendar(offset, month_length)
   lines = [ "日 月 火 水 木 金 土" ]
@@ -49,6 +22,18 @@ def format_calendar(offset, month_length)
   lines.join("\n")
 end
 
-string = _month.to_s + "月" + " "+ _year.to_s + "年"
-puts string.center(20, " ")
-puts format_calendar(x,number_of_days)
+def main(month, year)
+  month = check_month(month)
+  year = check_year(year)
+
+  number_of_days = Date.new(year, month, -1).day
+
+  start_day = Date.new(year, month, 1)
+  x = start_day.wday
+
+  string = month.to_s + "月" + " "+ year.to_s + "年"
+  puts string.center(20, " ")
+  puts format_calendar(x, number_of_days)
+end
+
+main(month, year)
