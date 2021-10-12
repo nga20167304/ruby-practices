064593109204fd5438a9e7c7e4ed65bcca3d6bc0
Nga <38340962+nga20167304@users.noreply.github.com>
2021-10-12 17:46:14 +0700

    fix bowling

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -4,29 +4,29 @@ score = ARGV[0]
 scores = score.split(',')
 shots = []
 scores.each do |s|
-  if s == 'X' # strike
+  if s == 'X'
     shots << 10
     shots << 0
   else
     shots << s.to_i
   end
 end
-p shots
 
-frames = []
-shots.each_slice(2) do |s|
-  frames << s
-end
-p frames
+frames = shots.each_slice(2).to_a
 
 point = 0
-frames.each do |frame|
-  point += if frame[0] == 10 # strike
-             30
-           elsif frame.sum == 10 # spare
-             frame[0] + 10
+frames.each_with_index do |frame, index|
+  break if index == 10
+
+  point += if frame[0] == 10 && frames[index + 1][0] == 10
+             20 + frames[index + 2][0]
+           elsif frame[0] == 10
+             10 + frames[index + 1].sum
+           elsif frame.sum == 10
+             10 + frames[index + 1][0]
            else
              frame.sum
            end
 end
+
 puts point
