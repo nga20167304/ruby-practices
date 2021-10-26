07d32f83daf9f1544825da70f66f259aee657526
Nga <38340962+nga20167304@users.noreply.github.com>
2021-10-26 21:55:02 +0700

    fix ls no options

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
+
+COLUMN_NUM = 3
 BLANK_NUM = 10
 
 def extract_elements
@@ -9,7 +11,7 @@ end
 def format(display_num_of_col, display_arr)
   slice_arr = handle_split_col(display_num_of_col, display_arr)
   length_string_max = string_length_max(display_arr)
-  arr_has_max_elements = slice_arr.max_by{|arr| arr.size}
+  arr_has_max_elements = slice_arr.max_by(&:size)
 
   (0...arr_has_max_elements.length).each do |i|
     tmp = ''
@@ -25,28 +27,28 @@ def handle_split_col(col_num, arr)
 
   max_num_of_row = num_of_row_max(col_num, arr)
 
-  arr.each_slice(max_num_of_row) {|a| array_result << a }
+  arr.each_slice(max_num_of_row) { |a| array_result << a }
 
   array_result
 end
 
 def num_of_row_max(col_num, arr)
-  return 0 if arr == []
+  return 0 if arr.empty?
 
   number_of_col_more = arr.length % col_num
-  min_num_of_row = (arr.length - number_of_col_more) / col_num
+  min_num_of_row = arr.length / col_num
 
   number_of_col_more.zero? ? min_num_of_row : min_num_of_row + 1
 end
 
 def string_length_max(arr)
-  string_max = arr.max_by { |x| x.length }
+  string_max = arr.max_by(&:length)
   string_max.length
 end
 
-def main(display_num_of_col)
+def main
   elements = extract_elements.sort
-  format(display_num_of_col, elements)
+  format(COLUMN_NUM, elements)
 end
 
-main(3)
+main
