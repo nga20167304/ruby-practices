a652ccf344bba49d82703fc7ca7c794cc39995c1
Nga <38340962+nga20167304@users.noreply.github.com>
2021-10-27 20:20:21 +0700

    change avariable and method name

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,47 +1,47 @@
 # frozen_string_literal: true
 
 COLUMN_NUM = 3
-BLANK_NUM = 10
+MARGIN = 10
 
 def extract_elements
   path = Dir.getwd
   Dir.entries(path).filter { |f| !f.start_with? '.' }
 end
 
-def format(display_arr)
-  slice_arr = handle_split_col(display_arr)
-  length_string_max = string_length_max(display_arr)
-  arr_has_max_elements = slice_arr.max_by(&:size)
+def format(extracted_elements)
+  columns = handle_split_columns(extracted_elements)
+  max_num_of_character = max_num_of_character(extracted_elements)
+  num_of_elements_of_longest_columns = columns.max_by(&:size)
 
-  (0...arr_has_max_elements.length).each do |i|
+  (0...num_of_elements_of_longest_columns.length).each do |i|
     tmp = ''
-    (0...slice_arr.length).each do |j|
-      tmp += slice_arr[j][i].to_s.ljust(length_string_max + BLANK_NUM)
+    (0...columns.length).each do |j|
+      tmp += columns[j][i].to_s.ljust(max_num_of_character + MARGIN)
     end
     puts tmp
   end
 end
 
-def handle_split_col(arr)
+def handle_split_columns(arr)
   array_result = []
 
-  max_num_of_row = num_of_row_max(arr)
+  max_num_of_row = max_num_of_per_column(arr)
 
   arr.each_slice(max_num_of_row) { |a| array_result << a }
 
   array_result
 end
 
-def num_of_row_max(arr)
+def max_num_of_per_column(arr)
   return 0 if arr.empty?
 
-  number_of_col_more = arr.length % COLUMN_NUM
-  min_num_of_row = arr.length / COLUMN_NUM
+  num_of_remainder = arr.length % COLUMN_NUM
+  max_num_of_per_column = arr.length / COLUMN_NUM
 
-  number_of_col_more.zero? ? min_num_of_row : min_num_of_row + 1
+  num_of_remainder.zero? ? max_num_of_per_column : max_num_of_per_column + 1
 end
 
-def string_length_max(arr)
+def max_num_of_character(arr)
   string_max = arr.max_by(&:length)
   string_max.length
 end
