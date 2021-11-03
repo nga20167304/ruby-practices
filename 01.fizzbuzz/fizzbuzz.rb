87f2637822a3b661f170e0139d01f2f2e55286a5
Nga <38340962+nga20167304@users.noreply.github.com>
2021-11-03 14:06:31 +0700

    rubocop for fizzbuzz and calendar

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,10 +1,12 @@
-def fizzbuzz num
+# frozen_string_literal: true
+
+def fizzbuzz(num)
   (1..num).each do |i|
-    if i % 3 == 0 && i % 5 == 0
+    if (i % 3).zero? && (i % 5).zero?
       puts 'FizzBuzz'
-    elsif i % 3 == 0
+    elsif (i % 3).zero?
       puts 'Fizz'
-    elsif i % 5 == 0
+    elsif (i % 5).zero?
       puts 'Buzz'
     else
       puts i
--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 require 'optparse'
 require 'date'
 
 def main
-  params = ARGV.getopts("m:","y:")
+  params = ARGV.getopts('m:', 'y:')
 
-  month = params["m"].to_i
-  year = params["y"].to_i
-  
+  month = params['m'].to_i
+  year = params['y'].to_i
+
   month = check_month(month)
   year = check_year(year)
 
@@ -15,21 +17,21 @@ def main
   start_day = Date.new(year, month, 1)
   x = start_day.wday
 
-  string = month.to_s + "月" + " "+ year.to_s + "年"
-  puts string.center(20, " ")
+  string = "#{month}月 #{year}年"
+  puts string.center(20, ' ')
   puts format_calendar(x, number_of_days)
 end
 
 def check_month(month)
-  month == 0 ? Date.today.month : month
+  month.zero? ? Date.today.month : month
 end
 
 def check_year(year)
-  year == 0 ? Date.today.year : year
+  year.zero? ? Date.today.year : year
 end
 
 def format_calendar(offset, month_length)
-  lines = [ "日 月 火 水 木 金 土" ]
+  lines = ['日 月 火 水 木 金 土']
   dates = [nil] * offset + (1..month_length).to_a
   dates.each_slice(7) do |week|
     lines << week.map { |date| date.to_s.rjust(3) }.join('')
